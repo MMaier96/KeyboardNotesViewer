96e0ca25f2775d685a663cbca1ff15ed0154cae1
marcel <marcelmaier.htc@gmail.com>
2018-02-08 23:10:03 +0100

    added basic functionallity of application

--- src/application/config/AppConfig.java
+++ src/application/config/AppConfig.java
@@ -1,10 +1,13 @@
-package application;
+package application.config;
 
 public enum AppConfig {
 	instance;
 	
+	public int pdfResolution = 120; //dpi
 	public int windowWidth = 500;
 	public int windowHeight = 500;
-	public boolean startAsFullScreen = true;
+	public boolean startAsFullScreen = false;
+	public String inputFolderName = "input/";
+	public String outputFolderName = "output/";
 	
 }
